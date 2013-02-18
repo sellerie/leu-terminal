376f86368d6abb7f4984c63b502472f025734820
Maik Brendler <sellerieschnitzel@googlemail.com>
2013-02-18 20:55:32 +0100

    Colorize output by html tag.

--- leo.hs
+++ leo.hs
@@ -1,11 +1,17 @@
 import System.Environment (getArgs)
+import System.Console.ANSI (setSGRCode,
+                            SGR(SetColor),
+                            ConsoleLayer(Foreground),
+                            ColorIntensity(Dull, Vivid),
+                            Color(Blue, Yellow))
+
 import Network.HTTP (simpleHTTP, getRequest, getResponseBody)
-import Text.XML.HaXml.Types (Content)
+
+import Text.XML.HaXml.Types (Content(..), Element(Elem), QName(N), Reference(..))
 import Text.XML.HaXml.Parse (xmlParse)
-import Text.XML.HaXml.Combinators (CFilter, tag, (/>), elm, txt, o)
+import Text.XML.HaXml.Combinators (CFilter, tag, (/>))
 import Text.XML.HaXml.Util (docContent)
 import Text.XML.HaXml.Posn (posInNewCxt)
-import Text.XML.HaXml.Html.Generate (htmlprint)
 
 
 buildLeoUrl :: String -> String
@@ -18,7 +24,7 @@ prettySections :: [Content i] -> String
 prettySections = showEntries . concat . (map parseSection)
   where
     showEntries = foldl (\m entry -> m ++ "\n" ++ showEntry entry) ""
-    showEntry (x, y) = show x ++ " : " ++ show y
+    showEntry (x, y) = x ++ " : " ++ y
 
 parseSection :: Content i -> [(String, String)]
 parseSection content = map entryToTuple $ tag "section" /> tag "entry" $ content
@@ -27,14 +33,32 @@ entryToTuple :: Content i -> (String, String)
 entryToTuple entry = (sideRepr side1, sideRepr side2)
   where (side1:side2:[]) = tag "entry" /> tag "side" $ entry
 
+clearSGR = setSGRCode []
+bSGR = setSGRCode [SetColor Foreground Vivid Blue]
+smallSGR = setSGRCode [SetColor Foreground Dull Yellow]
+
 sideRepr :: Content i -> String
-sideRepr side = show . htmlprint $ tag "side" /> tag "repr" $ side
+sideRepr side = concat $ map reprToString repr
+  where
+    repr = tag "side" /> tag "repr" $ side
+    contentsToString = concat . map reprToString
+    reprToString (CElem (Elem (N tag) _ subs) _) 
+      | tag == "repr" = contentsToString subs
+      | tag == "b" = bSGR ++ contentsToString subs ++ clearSGR
+      | tag == "small" = smallSGR ++ contentsToString subs ++ clearSGR
+      | tag == "i" = contentsToString subs
+      | otherwise = "unknown CElem: " ++ tag ++ " - " ++ contentsToString subs
+    reprToString (CString _ s _) = s
+    reprToString (CRef (RefEntity n) _) = "" -- "RefEntity: " ++ n
+    reprToString (CRef (RefChar n) _) = "" -- "RefChar: " ++ show n
+    reprToString (CMisc _ _) = "Misc"
 
 main :: IO ()
 main = do
   searchFor:_ <- getArgs
   query <- simpleHTTP . getRequest . buildLeoUrl $ searchFor
   queryResult <- getResponseBody query
+  -- queryResult <- readFile "test_data/query_for_hello.xml"
   let document = xmlParse "" queryResult
   let content = docContent (posInNewCxt "" Nothing) document
   let sections = sectionsFilter content
