08058fbc37df4f883052bed10b09bccb891160ad
Maik Brendler <sellerieschnitzel@googlemail.com>
2013-02-19 12:45:09 +0100

    Sort code in a better way.

--- leo.hs
+++ leo.hs
@@ -15,10 +15,6 @@ import Text.XML.HaXml.Combinators (CFilter, tag, (/>))
 import Text.XML.HaXml.Util (docContent)
 import Text.XML.HaXml.Posn (posInNewCxt)
 
-
-buildLeoUrl :: String -> String
-buildLeoUrl searchFor = "http://dict.leo.org/dictQuery/m-vocab/ende/query.xml?tolerMode=nof&lp=ende&lang=de&rmWords=off&rmSearch=on&search=" ++ searchFor ++ "&searchLoc=0&resultOrder=basic&multiwordShowSingle=on"
-
 sectionsFilter :: CFilter i
 sectionsFilter = tag "xml" /> tag "part" /> tag "section"
 
@@ -35,30 +31,39 @@ entryToTuple :: Content i -> (String, String)
 entryToTuple entry = (sideRepr side1, sideRepr side2)
   where (side1:side2:[]) = tag "entry" /> tag "side" $ entry
 
+sideRepr :: Content i -> String
+sideRepr side = concat $ map reprToString repr
+  where repr = tag "side" /> tag "repr" $ side
+
+contentsToString :: [Content i] -> String
+contentsToString = concat . map reprToString
+
+reprToString :: Content i -> String
+reprToString (CElem (Elem (N tagName) _ subs) _) 
+  | tagName == "repr" = contentsToString subs
+  | tagName == "b" = bSGR ++ contentsToString subs ++ clearSGR
+  | tagName == "small" = smallSGR ++ contentsToString subs ++ clearSGR
+  | tagName == "i" = contentsToString subs
+  | otherwise = "Not handled tagName: " ++ tagName ++ " (" ++ contentsToString subs ++ ")"
+reprToString (CElem (Elem (QN _ _) _ _) _) = "Not handled: QN"
+reprToString (CString _ s _) = s
+reprToString (CRef (RefEntity _) _) = ""  -- "RefEntity: " ++ n
+reprToString (CRef (RefChar _) _) = ""  -- "RefChar: " ++ show n
+reprToString (CMisc _ _) = "Misc"
+
+
 clearSGR :: String
 clearSGR = setSGRCode []
+
 bSGR :: String
 bSGR = setSGRCode [SetColor Foreground Vivid Blue]
+
 smallSGR :: String
 smallSGR = setSGRCode [SetColor Foreground Dull Yellow]
 
-sideRepr :: Content i -> String
-sideRepr side = concat $ map reprToString repr
-  where
-    repr = tag "side" /> tag "repr" $ side
-    contentsToString = concat . map reprToString
-    reprToString (CElem (Elem (N tagName) _ subs) _) 
-      | tagName == "repr" = contentsToString subs
-      | tagName == "b" = bSGR ++ contentsToString subs ++ clearSGR
-      | tagName == "small" = smallSGR ++ contentsToString subs ++ clearSGR
-      | tagName == "i" = contentsToString subs
-      | otherwise = "Not handled tagName: " ++ tagName ++ " (" ++ contentsToString subs ++ ")"
-    reprToString (CElem (Elem (QN _ _) _ _) _) = "Not handled: QN"
-    reprToString (CString _ s _) = s
-    reprToString (CRef (RefEntity _) _) = ""  -- "RefEntity: " ++ n
-    reprToString (CRef (RefChar _) _) = ""  -- "RefChar: " ++ show n
-    reprToString (CMisc _ _) = "Misc"
 
+buildLeoUrl :: String -> String
+buildLeoUrl searchFor = "http://dict.leo.org/dictQuery/m-vocab/ende/query.xml?tolerMode=nof&lp=ende&lang=de&rmWords=off&rmSearch=on&search=" ++ searchFor ++ "&searchLoc=0&resultOrder=basic&multiwordShowSingle=on"
 
 -- Maybe I need to get the Cookie by a regular call to dict.leo.org at first!
 addHeaders :: (Request a) -> (Request a)
