2a35ed5142695f39c5b27ff6bef66bedb6b88e32
Maik Brendler <sellerieschnitzel@googlemail.com>
2013-02-19 09:13:23 +0100

    Add comments about all http headers.

--- leo.hs
+++ leo.hs
@@ -58,6 +58,55 @@ sideRepr side = concat $ map reprToString repr
 
 
 -- Maybe I need to get the Cookie by a regular call to dict.leo.org at first!
+--
+-- All headers query.xml:
+--Request URL:http://dict.leo.org/dictQuery/m-vocab/ende/query.xml?tolerMode=nof&lp=ende&lang=de&rmWords=off&rmSearch=on&search=hallo&searchLoc=0&resultOrder=basic&multiwordShowSingle=on
+--Request Method:GET
+--Status Code:200 OK
+--Request Headersview source
+--Accept:text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8
+--Accept-Charset:ISO-8859-1,utf-8;q=0.7,*;q=0.3
+--Accept-Encoding:gzip,deflate,sdch
+--Accept-Language:de-DE,de;q=0.8,en-US;q=0.6,en;q=0.4
+--Cache-Control:max-age=0
+--Connection:keep-alive
+--Cookie:LEOABTEST=T; browser=webkit%3B5%3Bajax
+--Host:dict.leo.org
+--User-Agent:Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.57 Safari/537.17
+--Query String Parametersview sourceview URL encoded
+--tolerMode:nof
+--lp:ende
+--lang:de
+--rmWords:off
+--rmSearch:on
+--search:hallo
+--searchLoc:0
+--
+--
+-- All headers dict.leo.org
+--Request URL:http://dict.leo.org/
+--Request Method:GET
+--Status Code:304 Not Modified
+--Request Headersview source
+--Accept:text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8
+--Accept-Charset:ISO-8859-1,utf-8;q=0.7,*;q=0.3
+--Accept-Encoding:gzip,deflate,sdch
+--Accept-Language:de-DE,de;q=0.8,en-US;q=0.6,en;q=0.4
+--Cache-Control:max-age=0
+--Connection:keep-alive
+--Cookie:LEOABTEST=T; browser=webkit%3B5%3Bajax
+--Host:dict.leo.org
+--If-Modified-Since:Tue, 19 Feb 2013 08:04:40 GMT
+--User-Agent:Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.57 Safari/537.17
+--Response Headersview source
+--Cache-Control:max-age=10
+--Cache-Control:public, must-revalidate, proxy-revalidate
+--Connection:keep-alive
+--Date:Tue, 19 Feb 2013 08:11:49 GMT
+--Expires:Tue, 19 Feb 2013 08:11:59 GMT
+--Last-Modified:Tue, 19 Feb 2013 08:04:40 GMT
+--Server:nginx
+--
 --addHeaders :: (Request a) -> (Request a)
 addHeaders r = setHeaders r [Header HdrCookie "LEOABTEST=T; browser=webkit%3B5%3Bajax"]
 
