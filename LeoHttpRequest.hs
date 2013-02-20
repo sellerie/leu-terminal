03057be41e8a5880c38fbbba530078c39d6c26ac
Maik Brendler <sellerieschnitzel@googlemail.com>
2013-02-20 14:41:42 +0100

    Move HTTP request functions into own module.

---
+++ LeoHttpRequest.hs
@@ -0,0 +1,16 @@
+module LeoHttpRequest (searchWithHttp) where
+
+import Network.HTTP (simpleHTTP, getRequest, getResponseBody, Request)
+import Network.HTTP.Headers (setHeaders, Header(Header), HeaderName(HdrCookie))
+
+
+buildLeoUrl :: String -> String
+buildLeoUrl searchFor = "http://dict.leo.org/dictQuery/m-vocab/ende/query.xml?tolerMode=nof&lp=ende&lang=de&rmWords=off&rmSearch=on&search=" ++ searchFor ++ "&searchLoc=0&resultOrder=basic&multiwordShowSingle=on"
+
+-- Maybe I need to get the Cookie by a regular call to dict.leo.org at first!
+addHeaders :: Request a -> Request a
+addHeaders r = setHeaders r [Header HdrCookie "LEOABTEST=T; browser=webkit%3B5%3Bajax"]
+
+searchWithHttp :: String -> IO String
+searchWithHttp search = httpRequest search >>= getResponseBody
+  where httpRequest = simpleHTTP . addHeaders . getRequest . buildLeoUrl
--- leo.hs
+++ leo.hs
@@ -1,26 +1,13 @@
 import System.Environment (getArgs, getProgName)
 import Control.Monad (liftM2)
-import Codec.Binary.UTF8.String (decodeString)
 
-import Network.HTTP (simpleHTTP, getRequest, getResponseBody, Request)
-import Network.HTTP.Headers (setHeaders, Header(Header), HeaderName(HdrCookie))
+import Codec.Binary.UTF8.String (decodeString)
+import System.Console.ParseArgs (argsRest)
 
 import ParseLeo (xmlStringToParts)
 import PrettyPart (prettyPart)
 import CmdArgs (parseArguments, testFile, reverseOutput)
-import System.Console.ParseArgs (argsRest)
-
-
-buildLeoUrl :: String -> String
-buildLeoUrl searchFor = "http://dict.leo.org/dictQuery/m-vocab/ende/query.xml?tolerMode=nof&lp=ende&lang=de&rmWords=off&rmSearch=on&search=" ++ searchFor ++ "&searchLoc=0&resultOrder=basic&multiwordShowSingle=on"
-
--- Maybe I need to get the Cookie by a regular call to dict.leo.org at first!
-addHeaders :: Request a -> Request a
-addHeaders r = setHeaders r [Header HdrCookie "LEOABTEST=T; browser=webkit%3B5%3Bajax"]
-
-searchWithHttp :: String -> IO String
-searchWithHttp search = httpRequest search >>= getResponseBody
-  where httpRequest = simpleHTTP . addHeaders . getRequest . buildLeoUrl
+import LeoHttpRequest (searchWithHttp)
 
 
 main :: IO ()
@@ -32,4 +19,5 @@ main = do
 
   let parts = xmlStringToParts queryResult
   -- TODO: filter parts by command line options
-  putStrLn $ decodeString $ unlines $ map prettyPart $ reverseOutput opts parts
+  -- TODO: check if decodeString is really needed
+  putStrLn . decodeString . unlines $ map prettyPart $ reverseOutput opts parts
