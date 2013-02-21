a44c2e31983ed7777cb3079f4a86c1704dd836b0
Maik Brendler <sellerieschnitzel@googlemail.com>
2013-02-21 12:14:46 +0100

    Add a own ArgumentKey type instead of using Int.

--- CmdArgs.hs
+++ CmdArgs.hs
@@ -22,29 +22,31 @@ import System.Console.ParseArgs (
 
 data OutputFormat = Pretty | Xml deriving (Show)
 
+data ArgumentKeys = File | Reverse | XmlOut deriving (Show, Ord, Eq)
 
-allArguments :: [Arg Int]
+allArguments :: [Arg ArgumentKeys]
 allArguments =
   [
-    Arg 0 (Just 't') (Just "test-file") (argDataOptional "FILE" ArgtypeString)
+    Arg File (Just 't') (Just "test-file")
+        (argDataOptional "FILE" ArgtypeString)
         "work with FILE instead of a HTTP-Request"
-  , Arg 1 (Just 'r') (Just "reverse-output") Nothing
+  , Arg Reverse (Just 'r') (Just "reverse-output") Nothing
         "reverses the program output"
-  , Arg 2 (Just 'x') (Just "show-xml-response") Nothing
+  , Arg XmlOut (Just 'x') (Just "show-xml-response") Nothing
         "show XML response instead of parsed Translations"
   ]
 
-parseArguments :: String -> [String] -> Args Int
+parseArguments :: String -> [String] -> Args ArgumentKeys
 parseArguments = parseArgs (ArgsTrailing "SEARCH") allArguments
 
 
-testFile :: Args Int -> Maybe String
-testFile args = getArg args 0
+testFile :: Args ArgumentKeys-> Maybe String
+testFile args = getArg args File
 
 
-reverseOutput :: Args Int -> [a] -> [a]
-reverseOutput args = if gotArg args 1 then id else reverse
+reverseOutput :: Args ArgumentKeys  -> [a] -> [a]
+reverseOutput args = if gotArg args Reverse then id else reverse
 
 
-outputFormat :: Args Int -> OutputFormat
-outputFormat args = if gotArg args 2 then Xml else Pretty
+outputFormat :: Args ArgumentKeys -> OutputFormat
+outputFormat args = if gotArg args XmlOut then Xml else Pretty
