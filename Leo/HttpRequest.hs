61f4b7c0e21843e67358a37cdda4e08ff13aaced
Maik Brendler <sellerieschnitzel@googlemail.com>
2013-02-22 12:57:13 +0100

    Clean up buildLeoUrl.

--- Leo/HttpRequest.hs
+++ Leo/HttpRequest.hs
@@ -1,11 +1,25 @@
 module Leo.HttpRequest (searchWithHttp) where
 
+import Data.List (intercalate)
+
 import Network.HTTP (simpleHTTP, getRequest, getResponseBody, Request)
 import Network.HTTP.Headers (setHeaders, Header(Header), HeaderName(HdrCookie))
 
 
 buildLeoUrl :: String -> String
-buildLeoUrl searchFor = "http://dict.leo.org/dictQuery/m-vocab/ende/query.xml?tolerMode=nof&lp=ende&lang=de&rmWords=off&rmSearch=on&search=" ++ searchFor ++ "&searchLoc=0&resultOrder=basic&multiwordShowSingle=on"
+buildLeoUrl searchFor = url ++ "?" ++ intercalate "&" arguments
+  where
+    url = "http://dict.leo.org/dictQuery/m-vocab/ende/query.xml"
+    arguments = [
+        "tolerMode=nof"
+      , "lp=ende&lang=de"
+      , "rmWords=off"
+      , "rmSearch=on"
+      , "search=" ++ searchFor
+      , "searchLoc=0"
+      , "resultOrder=basic"
+      , "multiwordShowSingle=on"
+      ]  -- I do not know the meaning of all this arguments
 
 -- Maybe I need to get the Cookie by a regular call to dict.leo.org at first!
 addHeaders :: Request a -> Request a
